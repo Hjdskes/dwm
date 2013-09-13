6edd8d9bdee66f12103aae56c441f0195a7295cb
Unia <jthidskes@outlook.com>
2013-09-13 20:15:58 +0200

    implemented push

--- config.def.h
+++ config.def.h
@@ -64,6 +64,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_Up,     pushup,         {0} },
+	{ MODKEY,                       XK_Down,   pushdown,       {0} },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
