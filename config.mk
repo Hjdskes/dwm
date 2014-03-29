0cde3d62729e5c676e49cafbba6ba0c61950452e
Unia <jthidskes@outlook.com>
2014-03-29 22:58:40 +0100

    forgot these

--- config.mk
+++ config.mk
@@ -15,13 +15,13 @@ XINERAMALIBS  = -lXinerama
 XINERAMAFLAGS = -DXINERAMA
 
 # includes and libs
-INCS = -I${X11INC} `pkg-config --cflags xft pango pangoxft`
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} `pkg-config --libs xft pango pangoxft`
+INCS = -I${X11INC} `pkg-config --cflags pango pangocairo cairo`
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} `pkg-config --libs pango pangocairo cairo`
 
 # flags
 CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
+#CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = -s ${LIBS}
 
 # compiler and linker
