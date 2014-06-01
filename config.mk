3e7d8e5683157ee121a296f3e9e11c4e432a1c87
Jente Hidskes <hjdskes@gmail.com>
2014-06-02 00:30:07 +0200

    -Os causes segfault

--- config.mk
+++ config.mk
@@ -21,7 +21,7 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} `pkg-config --libs pangocairo`
 # flags
 CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall ${INCS} ${CPPFLAGS} #$-Os
 LDFLAGS  = -s ${LIBS}
 
 # compiler and linker
